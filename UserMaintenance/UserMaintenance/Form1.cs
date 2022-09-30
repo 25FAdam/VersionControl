a71e298e94c840c99dca38e813f9ea8c4b4f2ade
Adam Foldvari <25fadam@gmail.com>
2022-09-30 11:09:17 +0200

    savewrite

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,12 +15,13 @@ namespace UserMaintenance
     public partial class Form1 : Form
     {
         BindingList<User> users = new BindingList<User>();
+
         public Form1()
         {
             InitializeComponent();
             label1.Text = Resource1.FullName;            
             button1.Text = Resource1.Add;
-
+           
 
             // listbox1
             listBox1.DataSource = users;
@@ -34,6 +36,27 @@ namespace UserMaintenance
                 FullName = textBox1.Text,                
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.InitialDirectory = Application.StartupPath;
+
+            sfd.DefaultExt = "txt";
+            sfd.AddExtension = true;
+
+            if (sfd.ShowDialog()==DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8);
+
+                foreach(var u in users)
+                {
+                    sw.WriteLine($"{u.ID};{u.FullName}");
+                }
+                sw.Close();
+            }
         }
     }
 }
