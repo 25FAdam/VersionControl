0b799f3ffcbe05941481ed3b3e2b76e6db605f76
Adam Foldvari <25fadam@gmail.com>
2022-10-19 19:04:51 +0200

    Webszolgáltatás hívása

--- UserMaintenance/Otodik/Form1.cs
+++ UserMaintenance/Otodik/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Otodik.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,10 +12,32 @@ using System.Windows.Forms;
 namespace Otodik
 {
     public partial class Form1 : Form
-    {
+    {       
+     
         public Form1()
         {
             InitializeComponent();
+            Elso();
         }
+
+        public void Elso()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            
+            var response = mnbService.GetExchangeRates(request);
+
+           
+            var result = response.GetExchangeRatesResult;
+        }    
+
+   
     }
 }
