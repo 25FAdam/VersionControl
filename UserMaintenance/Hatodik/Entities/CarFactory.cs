a01f95b7fd7f51278575f275d01c9979ce62e172
Adam Foldvari <25fadam@gmail.com>
2022-10-29 15:41:42 +0200

    ItemSelectButtons

---
+++ UserMaintenance/Hatodik/Entities/CarFactory.cs
@@ -0,0 +1,22 @@
+﻿using Hatodik.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hatodik.Entities
+{
+    class CarFactory : IToyFactory
+    {
+        public Car CreateNew()
+        {
+            return new Car();
+        }
+
+        Toy IToyFactory.CreateNew()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
