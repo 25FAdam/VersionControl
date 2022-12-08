773285ab60cee158de4e7c116ce1658d470b516a
Adam Foldvari <25fadam@gmail.com>
2022-12-08 19:28:26 +0100

    playground

--- UserMaintenance/week10/Form1.cs
+++ UserMaintenance/week10/Form1.cs
@@ -13,11 +13,16 @@ namespace week10
 {
     public partial class Form1 : Form
     {
-       
+        GameController gc = new GameController();
+        GameArea ga;
+
         public Form1()
         {
             InitializeComponent();
-            
+
+            ga = gc.ActivateDisplay();
+            Controls.Add(ga);
+
         }
 
         
